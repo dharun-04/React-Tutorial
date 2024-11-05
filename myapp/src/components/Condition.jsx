137650607f32a0236b90d491c2d6b64845720bbd
dharun-04 <dharun.b2021ecea@sece.ac.in>
2024-11-05 09:38:45 +0530

    ternary operator

--- myapp/src/components/Condition.jsx
+++ myapp/src/components/Condition.jsx
@@ -4,5 +4,5 @@ import Exit from './Exit';
 
 export default function Condition() {
   const res=true;
-  return res?<h1>result is true</h1>:<h1>result is false</h1>;
+  return res?<h1>result is true</h1> : <h1>result is false</h1>;
 }
