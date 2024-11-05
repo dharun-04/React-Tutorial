274be035046896dd049ad374456fa8b9c60003aa
dharun-04 <dharun.b2021ecea@sece.ac.in>
2024-11-05 09:37:38 +0530

    conditional rendoring

--- myapp/src/components/Condition.jsx
+++ myapp/src/components/Condition.jsx
@@ -3,13 +3,6 @@ import Welcome from './welcome';
 import Exit from './Exit';
 
 export default function Condition() {
-  const res=false;
-  let message;
-  if(res){
-    message=<h1>Result is true</h1>
-  }
-  else{
-    message=<h2>Result is false</h2>
-  }
-  return message;
+  const res=true;
+  return res?<h1>result is true</h1>:<h1>result is false</h1>;
 }
