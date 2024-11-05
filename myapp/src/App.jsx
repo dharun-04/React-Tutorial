3d4a3b6638fdb4fbf93a9ad6b9045ee1a2b327aa
dharun-04 <dharun.b2021ecea@sece.ac.in>
2024-11-05 09:20:36 +0530

    basic condtion rendoring

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,9 +1,10 @@
+import Condition from "./components/Condition";
 import Fruits from "./components/Fruits";
 // import Hello from "./Hello"
 function App() {
   return (
     <>
-      <Fruits/>
+      <Condition />
     </>
   )
 }
---
+++ myapp/src/components/Condition.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Condition() {
+  const bo=false;
+  if(bo){
+    return <div>hello</div>;
+  }
+  else {
+    return <div>world</div>;
+  }
+}
