c4db7a5bda72aa8014721cb3b78d240209113d0d
dharun-04 <dharun.b2021ecea@sece.ac.in>
2024-11-05 09:31:28 +0530

    conditional rendoring using elements

--- myapp/src/components/Condition.jsx
+++ myapp/src/components/Condition.jsx
@@ -3,11 +3,13 @@ import Welcome from './welcome';
 import Exit from './Exit';
 
 export default function Condition() {
-  const bo=true;
-  if(bo){
-    return <Welcome/>;
+  const res=false;
+  let message;
+  if(res){
+    message=<h1>Result is true</h1>
   }
-  else {
-    return <Exit/>;
+  else{
+    message=<h2>Result is false</h2>
   }
+  return message;
 }
